352666a2f76b137e8f856388757337a20a15eb5a
Fatimah Ismael <fatimahismael@yahoo.com>
2023-04-30 12:43:53 -0700

    completed

--- index.js
+++ index.js
@@ -119,6 +119,8 @@ class Car {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
     // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -136,6 +138,17 @@ class Car {
    */
   drive(distance) {
     // ✨ implement
+    const milesCanDrive= this.tank * this.mpg
+    //this.odometer = this.odometer + distance
+    //return this.odometer
+    if (distance <= milesCanDrive){
+      this.odometer = this.odometer + distance
+      this.tank = this.tank - (distance/this.mpg)
+    } else {
+      this.odometer = this.odometer +milesCanDrive
+      this.tank = 0
+    }
+    return this.odometer
   }
 
   /**
@@ -151,6 +164,13 @@ class Car {
    */
   refuel(gallons) {
     // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if (gallons <= gallonsThatFit){
+      this.tank =this.tank + gallons
+    } else{
+      this.tank = this.tankSize
+    }
+    return this.tank *this.mpg
   }
 }
 
@@ -169,6 +189,8 @@ class Car {
  */
 function isEvenNumberAsync(number) {
   // ✨ implement
+ return number % 2 === 0 ? true : false
+    
 }
 
 module.exports = {
